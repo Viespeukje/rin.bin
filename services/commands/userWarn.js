855640bf4f7305d7d64b15db4d549623867f814f
Viespeukje <jennideffer@Jennifers-MacBook-Pro.local>
2018-01-09 10:29:03 -0800

    Warning tracker added to warning dialogue.

--- services/commands/userWarn.js
+++ services/commands/userWarn.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const
-    Discord = require("discord.js")
+    Discord = require("discord.js"),
+    mongoInsert = require('../database/mongoInsert')
+     
 
-module.exports = (client, message) => {
+module.exports = async (client, message) => {
 
     //Establishes mentioned user
-    var member = message.mentions.users.first();
+    let member = message.mentions.users.first();
  
     //If var member is "falsy" and no users are mentioned, fail immediately.
     if(!member){
@@ -16,18 +18,18 @@ module.exports = (client, message) => {
     }
 
     //Establishes the author of the message and the id of the member
-    var staffmember = message.author;
-    var memberID = member.id;
+    let staffmember = message.author;
+    let memberID = member.id;
 
     //Establishes Date Variables
-    var d = new Date();
+    let d = new Date();
     let dayvar = d.getDate();
     let monthvar = d.getMonth()+1; //Had to add a +1 because getMonth() returns 0-11
     let yearvar = d.getFullYear();
 
     //Getting usernames that aren't IDs to put in the table
-    var staffstring = staffmember.username +'%23'+ staffmember.discriminator; //"%23" is replaced in the URL by a # for the purposes of the form.
-    var memberstring = member.username +'%23'+ member.discriminator; //"%23" is replaced in the URL by a # for the purposes of the form.
+    let staffstring = staffmember.username +'%23'+ staffmember.discriminator; //"%23" is replaced in the URL by a # for the purposes of the form.
+    let memberstring = member.username +'%23'+ member.discriminator; //"%23" is replaced in the URL by a # for the purposes of the form.
 
     // //Otherwise, send the appropriate link.
     // if(member){
@@ -36,16 +38,24 @@ module.exports = (client, message) => {
     //     message.author.send(`Prefilled Link for User: ${member.username}\n\nhttps://docs.google.com/forms/d/e/1FAIpQLSc3ceOm39F1fdJq8VkmWEsJJW3rHXawaHoejP9G34xgX9GP4w/viewform?usp=pp_url&entry.640187248=${memberstring}&entry.867163136=${memberID}&entry.687949428=${staffstring}&entry.692760464=${yearvar}-${monthvar}-${dayvar}&entry.1364186442`);
     // }
 
+    const warningCount = await mongoInsert.warningLog(memberID);
 
     const embed = new Discord.RichEmbed()
-        .setAuthor(`Warning link for user [${member.username}#${member.discriminator}]`, member.avatarURL)
-        .setColor(15844367)
-        .addField(`Rin's Warning System`, `If you run into any issue with this form or record keeping, please let Rin know immediately and she can correct the mistake or delete the incorrect entries. Any entry flagged 'final warning' will be hilighted red automatically. Please consider a user's prior warnings when determining severity.`)
-        .addField('Click the link below to record a warning for this user...', `https://docs.google.com/forms/d/e/1FAIpQLSc3ceOm39F1fdJq8VkmWEsJJW3rHXawaHoejP9G34xgX9GP4w/viewform?usp=pp_url&entry.640187248=${memberstring}&entry.867163136=${memberID}&entry.687949428=${staffstring}&entry.692760464=${yearvar}-${monthvar}-${dayvar}&entry.1364186442\n`)
+        .setAuthor(`Warning Link for User "${member.username}#${member.discriminator}"`, member.avatarURL)
+
+    if(warningCount == 1) embed.setColor(9807270)
+    if(warningCount == 2) embed.addField(`Prior Warnings`,`One warning link has been generated for this user.`).setColor(15844367)
+    if(warningCount == 3) embed.addField(`Prior Warnings`,`Two warning links have been generated for this user.`).setColor(15105570)
+    if(warningCount >= 4) embed.addField(`Prior Warnings`,`Three or more  warning links have been generated for this user.`).setColor(15158332)
+
+
+
+        
+        
+    embed.addField('Click the link below to record a warning for this user...', `https://docs.google.com/forms/d/e/1FAIpQLSc3ceOm39F1fdJq8VkmWEsJJW3rHXawaHoejP9G34xgX9GP4w/viewform?usp=pp_url&entry.640187248=${memberstring}&entry.867163136=${memberID}&entry.687949428=${staffstring}&entry.692760464=${yearvar}-${monthvar}-${dayvar}&entry.1364186442\n`)
         .addField(`Click the link below to see all warnings recorded...`, `https://docs.google.com/spreadsheets/d/1PBd2cgpxZE9LdWqeEi39VY_2FOxGHPaGNy0l0bxs1i8/edit?usp=sharing`)
         .setFooter("I've been a good girl, right..?", client.user.avatarURL);
 
 
     message.author.send({embed}).catch(err => console.log("\x1b[31m%s\x1b[0m", `ERROR: Direct message send failed to user ${message.author.username}#${message.author.discriminator}. \n>>${err}`));
-
 }
--- services/database/mongoInsert.js
+++ services/database/mongoInsert.js
@@ -38,6 +38,47 @@ const guideMute = async function (messid, memid) {
 };
 
 
+const warningLog = async function (memid) {
+  let client;
+
+  try {
+    client = await MongoClient.connect(url);
+    console.log("---\nMongoInsert: Connected correctly to server...");
+
+    console.log(memid)
+
+    const db = client.db(dbName);
+        // Get the collection
+    const col = db.collection('warnings');
+    const docs = await col.find({memberid:memid}).next();
+    if (docs != null){
+      console.log(`Found user with existing warnings.`)
+      let warnings =  docs.warnCount+1;
+
+      await col.findOneAndUpdate({memberid : memid}, {$set: {warnCount: warnings}});   
+      console.log(`MongoInsert: warning for ${memid} with ${warnings} warnings correctly inserted...`);
+      return warnings;
+    }
+
+    else{
+      console.log(`Found user with no existing warnings.`)
+      await col.insertOne({memberid : memid, warnCount : 1});
+      let warnings = 1;
+      console.log(`MongoInsert: warning for ${memid} with 1 warnings correctly inserted...`);
+      return warnings;
+    }
+  } 
+  catch (err) {
+    console.log(err.stack);
+  }
+
+  // Close connection
+  client.close();
+  console.log("MongoInsert: Disconnected correctly from server...\n---");
+};
+
+
 module.exports = {
-    guideMute: guideMute
+    guideMute: guideMute,
+    warningLog: warningLog
 }
